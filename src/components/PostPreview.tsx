18717001872d9456d27e768b363bb8de2e2e6c24
Nicolas <divicolas@gmail.com>
2025-07-10 16:18:22 +0200

    chore: minor style improvements

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -10,17 +10,17 @@ type PostPreviewProps = {
 
 export default function PostPreview({ date, readTime, title, url, excerpt }: PostPreviewProps) {
   return (
-    <article>
+    <article className="flex flex-col items-start">
       <Link href={url} passHref>
-        <h1 className="text-text cursor-pointer text-lg font-semibold transition duration-500 ease-in-out hover:underline">
+        <h1 className="text-text cursor-pointer text-lg font-semibold transition-all duration-400 ease-in-out hover:underline">
           {title}
         </h1>
-        <p className="text-text-variant text-base font-normal">{excerpt}</p>
-        <span className="text-text-variant text-sm">
-          {new Date(date).toLocaleDateString("en-US", { month: "long", year: "numeric" })} &mdash;{" "}
-          {readTime} min read
-        </span>
       </Link>
+      <p className="text-text-variant text-base font-normal">{excerpt}</p>
+      <span className="text-text-variant text-sm">
+        {new Date(date).toLocaleDateString("en-US", { month: "long", year: "numeric" })} &mdash;{" "}
+        {readTime} min read
+      </span>
     </article>
   );
 }
