a32c6ded4cc4b481338d8f33babc9b5693c01587
Nicolas <divicolas@gmail.com>
2025-07-10 13:05:18 +0200

    fix: generate images using new function

--- src/app/adventures/[slug]/opengraph-image.tsx
+++ src/app/adventures/[slug]/opengraph-image.tsx
@@ -1,22 +1,21 @@
-import { ImageResponse } from "next/og";
 import { getPost, ADVENTURES_PATH } from "@/lib/api";
+import { generateOgImage } from "@/lib/og";
 
-// Image metadata
+// metadata
 export const size = {
   width: 1200,
   height: 630,
 };
-
 export const contentType = "image/png";
 
 export default async function Image({ params }: { params: { slug: string } }) {
   const post = await getPost(ADVENTURES_PATH, params.slug);
 
-  return new ImageResponse(
-    (
-      <div className="flex h-full w-full justify-start bg-gradient-to-tr from-gray-900 via-slate-800 to-indigo-500 text-white">
-        <div className="text-md h-full w-0.5 font-bold text-white">{post.metadata.title}</div>
-      </div>
-    )
-  );
+  return await generateOgImage({
+    title: post.metadata.title,
+    size: {
+      width: size.width,
+      height: size.height,
+    },
+  });
 }
