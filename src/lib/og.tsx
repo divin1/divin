a78f819a95504c05d8b7620e31739607d4ac57b1
Nicolas <divicolas@gmail.com>
2025-12-22 12:04:38 +0100

    chore: formatting

--- src/lib/og.tsx
+++ src/lib/og.tsx
@@ -14,16 +14,14 @@ export async function generateOgImage({ title, size }: OgImageProps) {
   const fontData = await readFile(join(process.cwd(), "public/assets/Inter-Bold.ttf"));
 
   return new ImageResponse(
-    (
-      <div tw="w-full h-full flex flex-col bg-[#0a0a0a] text-[#ededed] justify-between p-10">
-        <div tw="flex flex-col">
-          <div tw="mt-10 text-6xl font-bold leading-[6rem] px-0 pb-24 tracking-tight text-[#ededed]">
-            {title}
-          </div>
+    <div tw="w-full h-full flex flex-col bg-[#0a0a0a] text-[#ededed] justify-between p-10">
+      <div tw="flex flex-col">
+        <div tw="mt-10 text-6xl font-bold leading-[6rem] px-0 pb-24 tracking-tight text-[#ededed]">
+          {title}
         </div>
-        <div tw="text-2xl font-bold text-text flex justify-end w-full">divin.me</div>
       </div>
-    ),
+      <div tw="text-2xl font-bold text-text flex justify-end w-full">divin.me</div>
+    </div>,
     {
       ...size,
       fonts: [
