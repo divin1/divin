5d19e449089f2dd3ee60ee3cefd1270d5936e46e
Nicolas <divicolas@gmail.com>
2026-01-09 15:49:55 +0100

    chore: improve content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,37 +9,35 @@ export default function About() {
         </p>
         <p>
           Today, I&rsquo;m based in Bern, working as full-stack engineer at a startup that&rsquo;s
-          on a mission to bring real innovation to one of the most traditional, slow-moving
-          industries out there. We&rsquo;re using technology to challenge the status quo and
-          it&rsquo;s as exciting as it sounds.
+          on a mission to bring innovation to one of the most traditional, slow-moving industries
+          out there. We&rsquo;re using technology to challenge the status quo and it&rsquo;s as
+          exciting as it sounds.
         </p>
         <p>
           From a young age, I&rsquo;ve been driven by curiosity. Whether it was taking apart Lego
           sets, trying to understand machines and computers, or simply figuring out what makes
-          people tick, I&rsquo;ve always loved learning how things work, and while my early
-          experiments were fueled by spontaneous bursts of interest, I later discovered that focus
-          can transform that instinctive and sporadic drive into something truly valuable.
+          people tick, I&rsquo;ve always loved learning how things work.
         </p>
         <p>
-          Computer Science and the digital world instantly resonated with my need for expression.
-          It&rsquo;s a fast-evolving, fluid space, constantly shifting and expanding. It offers
-          endless opportunities for discovery, continuous learning, and technical challenges, all
-          while allowing for unlimited freedom in creativity and self-expression. There&rsquo;s
+          Computer Science and its digital world instantly resonated with my need for expression.
+          It&rsquo;s a vast, fast-evolving, fluid space, constantly shifting and expanding. It
+          offers endless opportunities for discovery, continuous learning, and technical challenges,
+          all while allowing for unlimited freedom in creativity and self-expression. There&rsquo;s
           something uniquely empowering about sitting down at a desk, opening a laptop, and being
           able to build something valuable from nothing &mdash; well, almost nothing.
         </p>
         <p>
           Outside of work, you&rsquo;ll most likely find me in the mountains. I&rsquo;ve always been
           drawn to nature and the outdoors, especially mountains who as time has passed have become
-          my most beloved playground. I&rsquo;m passionate about outdoor sports like trail running
-          and climbing &mdash; and while I might describe myself as a <i>type 2 fun</i> enjoyer,
+          my most beloved playground. I enjoy outdoor sports like trail running, alpinism and
+          climbing &mdash; and while I might describe myself as a <i>type 2 fun</i> enjoyer,
           it&rsquo;s not just for the physical challenge, but mostly as a way to explore more of the
-          world. I see movement as a means to discovery.
+          alpine world I love. I see movement more as a means to discovery, rather than a sport and
+          the end goal itself.
         </p>
         <p>
           I created this site to share my journey through the projects I&rsquo;m working on, ideas
-          I&rsquo;m exploring, things I&rsquo;m learning, and, of course, the occasional adventure
-          story.
+          I&rsquo;m exploring and lessons I&rsquo;m learn.
         </p>
 
         <p className="mt-5">
@@ -59,8 +57,7 @@ export default function About() {
           <br />
           It all started back in university, where &rdquo;divin1&rdquo; was the shorthand assigned
           to me for accessing internal systems. I liked the sound of it, and when I saw it was
-          available on GitHub, I grabbed divin1. Later, I went with divin for my website, it just
-          stuck.
+          available on GitHub, I grabbed it. Later, I went with divin for my website, it just stuck.
         </p>
       </div>
     </div>
